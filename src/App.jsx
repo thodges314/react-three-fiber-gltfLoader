793c586e734ff3558c764da9a3a01b95efdf317f
Thomas Hodges <thodges@javascriptonline.org>
2023-02-09 16:22:14 -0800

    shanghai monk

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,7 @@ const App = () => {
   const gltf = useLoader(GLTFLoader, "./models/monkeyShine.glb");
   return (
     <Canvas shadows camera={{ position: [-0.5, 1, 2] }}>
-      <Environment
-        files="./img/dikhololo_night_1k.hdr"
-        background
-        blur={0.05}
-      />
+      <Environment files="./img/shanghai_bund_1k.hdr" background blur={0.05} />
       <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
       <primitive object={gltf.scene} position={[0, 1, 0]} />
 
